46a88857f5ed17b0493e02568575d5fbe7048990
林维然 <weiran.lin@emc.top>
2018-05-24 15:34:17 +0800

    edit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import reduxPromise from 'redux-promise';
 import './i18n';
 import App from './App.js';
 import reducers from './reducers';
@@ -20,7 +20,7 @@ import reducers from './reducers';
     window.onresize = setRootFontSize;
 }(750));
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(reduxPromise));
 
 ReactDom.render(
     <Provider store={store}>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 // import { I18n } from 'react-i18next';
 // import styles from './home.scss';
 
 // import ReactDom from 'react-dom';
 
 // console.log(styles);
-export default function App() {
-    // const t2 = '123';
-    return (
-        <div>111</div>
-    );
+console.log(PropTypes);
+function mapStateToProps(state) {
+    return state;
 }
+
+@connect(mapStateToProps)
+export default class App extends Component {
+    static propTypes = {
+        test: PropTypes.number
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            d: 11111
+        };
+    }
+    // const t2 = '123';
+    render() {
+        const { test } = this.props;
+        console.log(test);
+        return <div>1231212{this.state.d}</div>;
+    }
+}
+
+App.defaultProps = {
+    test: 1
+};
